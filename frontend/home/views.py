14517844de9df552ec2d913172881599816db4e4
Đặng Phương Nam <namdang@vinades.vn>
2024-05-16 22:55:20 +0700

    chore: add file

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -1,88 +1,17 @@
 from __future__ import print_function
 import os
-from django.http import JsonResponse
-from django.shortcuts import render
-from django.views import View
-from collections import Counter
-from keras.models import load_model
-from transformers import pipeline
-from nltk import ngrams
-import numpy as np
-import re
-import string
-
 import sys
+from django.views import View
+from transformers import pipeline
+from django.shortcuts import render
+from django.http import JsonResponse
 
 if not sys.warnoptions:
     import warnings
 
     warnings.simplefilter("ignore")
 
-
-def extract_phrases(text):
-    pattern = r'\w[\w ]*|\s\W+|\W+'
-    return re.findall(pattern, text)
-
-
-def encoder_data(text, maxlen):
-    text = "\x00" + text
-    x = np.zeros((maxlen, len(alphabet)))
-    for i, c in enumerate(text[:maxlen]):
-        x[i, alphabet.index(c)] = 1
-    if i < maxlen - 1:
-        for j in range(i + 1, maxlen):
-            x[j, 0] = 1
-    return x
-
-
-def decoder_data(x):
-    x = x.argmax(axis=-1)
-    return ''.join(alphabet[i] for i in x)
-
-
-def nltk_ngrams(words, n=5):
-    return ngrams(words.split(), n)
-
-
-def guess(ngram):
-    text = ' '.join(ngram)
-    preds = model.predict(np.array([encoder_data(text, MAXLEN)]), verbose=0)
-    return decoder_data(preds[0]).strip('\x00')
-
-
-def correct(sentence):
-    for i in sentence:
-        if i not in accepted_char:
-            sentence = sentence.replace(i, " ")
-    ngrams = list(nltk_ngrams(sentence, n=NGRAM))
-    guessed_ngrams = list(guess(ngram) for ngram in ngrams)
-    candidates = [Counter() for _ in range(len(guessed_ngrams) + NGRAM - 1)]
-    for nid, ngram in (enumerate(guessed_ngrams)):
-        for wid, word in (enumerate(re.split(' +', ngram))):
-            candidates[nid + wid].update([word])
-
-    output = ' '.join(c.most_common(1)[0][0] for c in candidates)
-    return output
-
-
-NGRAM = 5
-MAXLEN = 40
-alphabet = ['\x00', ' ', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', 'á', 'à', 'ả', 'ã', 'ạ', 'â', 'ấ', 'ầ', 'ẩ', 'ẫ', 'ậ', 'ă', 'ắ', 'ằ', 'ẳ', 'ẵ', 'ặ', 'ó', 'ò',
-            'ỏ', 'õ', 'ọ', 'ô', 'ố', 'ồ', 'ổ', 'ỗ', 'ộ', 'ơ', 'ớ', 'ờ', 'ở', 'ỡ', 'ợ', 'é', 'è', 'ẻ', 'ẽ', 'ẹ', 'ê',
-            'ế', 'ề', 'ể', 'ễ', 'ệ', 'ú', 'ù', 'ủ', 'ũ', 'ụ', 'ư', 'ứ', 'ừ', 'ử', 'ữ', 'ự', 'í', 'ì', 'ỉ', 'ĩ', 'ị',
-            'ý', 'ỳ', 'ỷ', 'ỹ', 'ỵ', 'đ', 'Á', 'À', 'Ả', 'Ã', 'Ạ', 'Â', 'Ấ', 'Ầ', 'Ẩ', 'Ẫ', 'Ậ', 'Ă', 'Ắ', 'Ằ', 'Ẳ',
-            'Ẵ', 'Ặ', 'Ó', 'Ò', 'Ỏ', 'Õ', 'Ọ', 'Ô', 'Ố', 'Ồ', 'Ổ', 'Ỗ', 'Ộ', 'Ơ', 'Ớ', 'Ờ', 'Ở', 'Ỡ', 'Ợ', 'É', 'È',
-            'Ẻ', 'Ẽ', 'Ẹ', 'Ê', 'Ế', 'Ề', 'Ể', 'Ễ', 'Ệ', 'Ú', 'Ù', 'Ủ', 'Ũ', 'Ụ', 'Ư', 'Ứ', 'Ừ', 'Ử', 'Ữ', 'Ự', 'Í',
-            'Ì', 'Ỉ', 'Ĩ', 'Ị', 'Ý', 'Ỳ', 'Ỷ', 'Ỹ', 'Ỵ', 'Đ']
-letters = list(
-    "abcdefghijklmnopqrstuvwxyzáàảãạâấầẩẫậăắằẳẵặóòỏõọôốồổỗộơớờởỡợéèẻẽẹêếềểễệúùủũụưứừửữựíìỉĩịýỳỷỹỵđABCDEFGHIJKLMNOPQRSTUVWXYZÁÀẢÃẠÂẤẦẨẪẬĂẮẰẲẴẶÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÉÈẺẼẸÊẾỀỂỄỆÚÙỦŨỤƯỨỪỬỮỰÍÌỈĨỊÝỲỶỸỴĐ")
-accepted_char = list((string.digits + ''.join(letters)))
-
-model = load_model("/home/namd/Them_dautv/spell.h5")
-corrector = pipeline("text2text-generation", model="bmd1905/vietnamese-correction")
+model = pipeline("text2text-generation", model="bmd1905/vietnamese-correction")
 
 
 # Create your models here.
@@ -100,8 +29,7 @@ class IndexView(View):
 
     def post(self, request):
         desc = request.POST['desciption']
-        desc = correct(desc)
-        sentence = corrector(desc, max_length=30)[0]['generated_text']
+        sentence = model(desc, max_length=30)[0]['generated_text']
 
         data = {
             'vi_text': desc,
